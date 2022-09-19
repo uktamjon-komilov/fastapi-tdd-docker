6e99a29fe73f828d505d438517e40be37a013d5b
Uktamjon Komilov <uktamjon_komillov@epam.com>
2022-09-19 20:07:39 +0500

    other routes with tdd

--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,10 +1,14 @@
 from tortoise import models, fields
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class TextSummary(models.Model):
     url = fields.TextField()
     summary = fields.TextField()
-    created_at = fields.DatetimeField()
+    created_at = fields.DatetimeField(auto_now_add=True)
 
     def __str__(self):
         return self.url
+
+
+SummarySchema = pydantic_model_creator(TextSummary)
