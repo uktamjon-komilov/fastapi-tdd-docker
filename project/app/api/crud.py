69ac2044721b19d14d077437b5702e9a9e6555bf
Uktamjon Komilov <uktamjon_komillov@epam.com>
2022-09-29 20:03:05 +0500

    finished remaining routes with tests

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,6 +1,7 @@
 from typing import List, Union
 
-from app.models.pydantic import SummaryPayloadSchema
+from app.models.pydantic import (SummaryPayloadSchema,
+                                 SummaryUpdatePayloadSchema)
 from app.models.tortoise import TextSummary
 
 
@@ -22,3 +23,20 @@ async def get(id: int) -> Union[dict, None]:
 async def get_all() -> List:
     summaries = await TextSummary.all().values()
     return summaries
+
+
+async def delete(id: int) -> int:
+    summary = await TextSummary.filter(id=id).first().delete()
+    return summary
+
+
+async def put(id: int, payload: SummaryUpdatePayloadSchema) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).update(
+        url=payload.url, summary=payload.summary
+    )
+
+    if summary is not None:
+        updated_summary = await TextSummary.filter(id=id).first()
+        return updated_summary
+
+    return None
